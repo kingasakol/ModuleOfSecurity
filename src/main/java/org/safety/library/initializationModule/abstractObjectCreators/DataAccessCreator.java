be507dd7380abe42bbda7c81326b25700b441d9c
Krzysztof Gądek <305482@student.agh.edu.pl>
2022-01-05 01:11:11 +0100

    set Role to the Hibernate entity

--- src/main/java/org/safety/library/initializationModule/abstractObjectCreators/DataAccessCreator.java
+++ src/main/java/org/safety/library/initializationModule/abstractObjectCreators/DataAccessCreator.java
@@ -1,7 +1,14 @@
 package org.safety.library.initializationModule.abstractObjectCreators;
 
+import org.hibernate.Session;
+import org.safety.library.hibernate.SessionProvider;
 import org.safety.library.initializationModule.JSONMappingFactory;
 import org.safety.library.initializationModule.abstractMappingObjects.DataAccess;
+import org.safety.library.models.Role;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class DataAccessCreator {
     private String jsonPath;
@@ -11,8 +18,27 @@ public class DataAccessCreator {
     }
 
     public DataAccess createDataAccess(JSONMappingFactory factory){
-        //TODO
+        Map<String, Map<Role, List<Integer>>> result = new HashMap<>();
+        List<List<String>> readData = null;
+        try{
+            factory.read(jsonPath).getMappedData().forEach(innerList -> {
+
+            });
+
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+
         return null;
     }
+
+    private Map<String, Role> getRolesByItsNames(){
+        Session session = SessionProvider.getSession();
+        Map<String, Role> result = new HashMap<>();
+        session.createQuery("FROM Role R");
+        return result;
+    }
+
 
 }
--- src/main/java/org/safety/library/models/Role.java
+++ src/main/java/org/safety/library/models/Role.java
@@ -1,7 +1,25 @@
 package org.safety.library.models;
 
+import javax.persistence.*;
+
+@Entity
 public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
     private String name;
+
+    public Role() {}
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
 
     public Role(String name) {
         this.name = name;
