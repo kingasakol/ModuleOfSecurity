21cd766666639446181c8028b41f3d6cf000e4e4
Krzysztof Gądek <305482@student.agh.edu.pl>
2022-01-06 23:53:49 +0100

    implemented EntityAccessUser

--- src/main/java/org/safety/library/initializationModule/abstractMappingUsers/EntityAccessUser.java
+++ src/main/java/org/safety/library/initializationModule/abstractMappingUsers/EntityAccessUser.java
@@ -1,10 +1,32 @@
 package org.safety.library.initializationModule.abstractMappingUsers;
 
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+import org.safety.library.hibernate.SessionProvider;
 import org.safety.library.initializationModule.JSONMappingUser;
+import org.safety.library.initializationModule.abstractMappingObjects.EntityAccess;
+import org.safety.library.models.AccessListRow;
+import org.safety.library.models.AddPrivilege;
 
 public class EntityAccessUser implements JSONMappingUser {
+
+    private EntityAccess entityAccess;
+
+    public EntityAccessUser(EntityAccess entityAccess){
+        this.entityAccess = entityAccess;
+    }
+
     @Override
     public void use() {
-        //TODO
+        this.entityAccess.getAddPriviledges().forEach(((role, tableNames) -> {
+            tableNames.forEach(tableName -> {
+                Session session = SessionProvider.getSession();
+                Transaction tx = session.beginTransaction();
+                AddPrivilege addPrivilege = new AddPrivilege(role, tableName);
+                session.save(addPrivilege);
+                tx.commit();
+                session.close();
+            });
+        }));
     }
 }
