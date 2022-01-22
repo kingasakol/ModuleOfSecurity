d783f52f9ff379b187a2eec9a4ee96e94783cef5
Bartłomiej <kiszka2200@onet.pl>
2022-01-22 16:50:26 +0100

    TEST

--- src/test/java/initializationModule/abstractObjectCreators/RolesListCreatorTest.java
+++ src/test/java/initializationModule/abstractObjectCreators/RolesListCreatorTest.java
@@ -8,9 +8,11 @@ import org.safety.library.initializationModule.abstractMappingObjects.RolesList;
 import org.safety.library.initializationModule.abstractMappingUsers.RolesListUser;
 import org.safety.library.initializationModule.abstractObjectCreators.RolesListCreator;
 import org.safety.library.initializationModule.mappingFactories.RolesListJSONMapping;
+import org.safety.library.models.DefaultPrivilige;
 
 import java.net.URISyntaxException;
 import java.nio.file.Paths;
+import java.util.List;
 
 import static org.junit.Assert.assertEquals;
 
@@ -36,16 +38,53 @@ public class RolesListCreatorTest {
             assertEquals(rolesList.getRoles().get(0).getName(), "admin");
             assertEquals(rolesList.getRoles().get(1).getName(), "ksiegowy");
             assertEquals(rolesList.getRoles().get(2).getName(), "robol");
-//
-//            assertEquals(rolesList.getRoles().get(0).get(1),"Klasa");
-//            assertEquals(rolesList.getRoles().get(0).get(2),"true");
-//            assertEquals(rolesList.getRoles().get(0).get(3),"true");
-//            assertEquals(rolesList.getRoles().get(0).get(4),"true");
-//
-//            assertEquals(rolesList.getRoles().get(0).get(5),"InnaKlasa");
-//            assertEquals(rolesList.getRoles().get(0).get(6),"true");
-//            assertEquals(rolesList.getRoles().get(0).get(7),"true");
-//            assertEquals(rolesList.getRoles().get(0).get(8),"true");
+
+            List<DefaultPrivilige> adminPrivs = rolesList.getRoles().get(0).getDefaultPriviliges();
+            List<DefaultPrivilige> ksiegowyPrivs = rolesList.getRoles().get(1).getDefaultPriviliges();
+            List<DefaultPrivilige> robolPrivs = rolesList.getRoles().get(2).getDefaultPriviliges();
+
+            DefaultPrivilige adminKlasa = adminPrivs.get(0);
+            DefaultPrivilige adminInnaKlasa = adminPrivs.get(1);
+
+            DefaultPrivilige ksiegowyKlasa = ksiegowyPrivs.get(0);
+            DefaultPrivilige ksiegowyInnaKlasa = ksiegowyPrivs.get(1);
+
+            DefaultPrivilige robolKlasa = robolPrivs.get(0);
+            DefaultPrivilige robolInnaKlasa = robolPrivs.get(1);
+            // asserting role names are correct
+            assertEquals(adminKlasa.getRole().getName(), "admin");
+            assertEquals(adminInnaKlasa.getRole().getName(), "admin");
+
+            assertEquals(ksiegowyKlasa.getRole().getName(), "ksiegowy");
+            assertEquals(ksiegowyInnaKlasa.getRole().getName(), "ksiegowy");
+
+            assertEquals(robolKlasa.getRole().getName(), "robol");
+            assertEquals(robolInnaKlasa.getRole().getName(), "robol");
+            // asserting containing priviliges are valid
+            assertEquals(adminKlasa.isCanDelete(), true);
+            assertEquals(adminKlasa.isCanRead(), true);
+            assertEquals(adminKlasa.isCanUpdate(), true);
+
+            assertEquals(adminInnaKlasa.isCanDelete(), true);
+            assertEquals(adminInnaKlasa.isCanRead(), true);
+            assertEquals(adminInnaKlasa.isCanUpdate(), true);
+
+
+            assertEquals(ksiegowyKlasa.isCanDelete(), true);
+            assertEquals(ksiegowyKlasa.isCanRead(), false);
+            assertEquals(ksiegowyKlasa.isCanUpdate(), false);
+
+            assertEquals(ksiegowyInnaKlasa.isCanDelete(), true);
+            assertEquals(ksiegowyInnaKlasa.isCanRead(), true);
+            assertEquals(ksiegowyInnaKlasa.isCanUpdate(), false);
+
+            assertEquals(robolKlasa.isCanDelete(), true);
+            assertEquals(robolKlasa.isCanRead(), false);
+            assertEquals(robolKlasa.isCanUpdate(), false);
+
+            assertEquals(robolInnaKlasa.isCanDelete(), true);
+            assertEquals(robolInnaKlasa.isCanRead(), false);
+            assertEquals(robolInnaKlasa.isCanUpdate(), false);
 
 
 //            Don't know if that below should work bc there is issue with database
